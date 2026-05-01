ae0b9249e712af31b1ac9491c7b3cf0792bf5ada
Yazan-hassouneh <yazanhassouneh8@gmail.com>
2026-05-01 18:38:05 +0300

    Update_Style

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-})
+});
